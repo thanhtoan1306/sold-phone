539a4c6607160c106ed51ea09b8b3d8a0de8a65b
Nghia258 <58476591+Nghia258@users.noreply.github.com>
2020-06-01 15:01:05 +0700

    Chỉnh lỗi phần vừa pull

--- src/com/qlchdt/dao/TaiKhoanDao.java
+++ src/com/qlchdt/dao/TaiKhoanDao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Dao;
+package com.qlchdt.dao;
 
 import com.qlchdt.model.TaiKhoan;
 import java.sql.Connection;
