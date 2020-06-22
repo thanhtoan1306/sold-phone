89d40dd47f644f6b9136d645aea24029fbb96b4a
toankikyo <toan190500@gmail.com>
2020-06-22 07:34:25 +0700

    sửa xuất excel phiếu nhập

--- src/BUS/DIO/XuatExcel.java
+++ src/BUS/DIO/XuatExcel.java
@@ -6,17 +6,21 @@
 package BUS.DIO;
 
 import BUS.ChiTietHoaDonService;
+import BUS.ChiTietPhieuNhapService;
 import BUS.HoaDonService;
 import BUS.KhachHangService;
 import BUS.KhuyenMaiService;
 import BUS.NhaCungCapService;
 import BUS.NhanVienService;
+import BUS.PhieuNhapService;
 import BUS.SanPhamService;
 import DTO.Model.ChiTietHoaDon;
+import DTO.Model.ChiTietPhieuNhap;
 import DTO.Model.HoaDon;
 import DTO.Model.KhachHang;
 import DTO.Model.KhuyenMai;
 import DTO.Model.NhaCungCap;
+import DTO.Model.PhieuNhap;
 import java.awt.FileDialog;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -284,8 +288,8 @@ public class XuatExcel {
 
             row.createCell(8, CellType.STRING).setCellValue("Sản phẩm");
             row.createCell(9, CellType.STRING).setCellValue("Số lượng");
-            row.createCell(10, CellType.STRING).setCellValue("Đơn giá");
-            row.createCell(11, CellType.STRING).setCellValue("Thành tiền");
+            row.createCell(10, CellType.STRING).setCellValue("Đơn giá(triệu)");
+            row.createCell(11, CellType.STRING).setCellValue("Thành tiền(triệu");
 
             for (HoaDon hd : list) {
                 rownum++;
@@ -311,6 +315,95 @@ public class XuatExcel {
                     row.createCell(9, CellType.NUMERIC).setCellValue(cthd.getSoLuong());
                     row.createCell(10, CellType.NUMERIC).setCellValue(cthd.getDonGia());
                     row.createCell(11, CellType.NUMERIC).setCellValue(cthd.getDonGia() * cthd.getSoLuong());
+                }
+            }
+            for (int i = 0; i < rownum; i++) {
+                sheet.autoSizeColumn(i);
+            }
+
+            File file = new File(url);
+            file.getParentFile().mkdirs();
+            outFile = new FileOutputStream(file);
+            workbook.write(outFile);
+
+            JOptionPane.showMessageDialog(null, "Ghi file thành công: " + file.getAbsolutePath());
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(XuatExcel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(XuatExcel.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (outFile != null) {
+                    outFile.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(XuatExcel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    // xuất excel phiếu nhập
+    public void xuatFileExcelPhieuNhap() {
+        fd.setTitle("Xuất dữ liệu phiếu nhập ra excel");
+        String url = getFile();
+        if (url == null) {
+            return;
+        }
+
+        FileOutputStream outFile = null;
+        try {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            HSSFSheet sheet = workbook.createSheet("Phiếu nhập");
+
+            PhieuNhapService qlpnBUS = new PhieuNhapService();
+            ChiTietPhieuNhapService qlctpnBUS = new ChiTietPhieuNhapService();
+            NhanVienService qlnvBUS = new NhanVienService();
+            KhachHangService qlkhBUS = new KhachHangService();
+            KhuyenMaiService qlkmBUS = new KhuyenMaiService();
+            SanPhamService qlspBUS = new SanPhamService();
+            ArrayList<PhieuNhap> list = qlpnBUS.getDspn();
+
+            int rownum = 0;
+            int sttPhieuNhap = 0;
+            Row row = sheet.createRow(rownum);
+
+            row.createCell(0, CellType.NUMERIC).setCellValue("STT");
+            row.createCell(1, CellType.STRING).setCellValue("Mã phiếu nhập");
+            row.createCell(2, CellType.STRING).setCellValue("Mã nhà cung cấp");
+            row.createCell(3, CellType.STRING).setCellValue("Mã nhân viên");
+            row.createCell(4, CellType.STRING).setCellValue("Ngày Nhập");
+            row.createCell(5, CellType.STRING).setCellValue("Giờ Nhập");
+            row.createCell(6, CellType.STRING).setCellValue("Tổng tiền");
+
+            row.createCell(7, CellType.STRING).setCellValue("Sản phẩm");
+            row.createCell(8, CellType.STRING).setCellValue("Số lượng");
+            row.createCell(9, CellType.STRING).setCellValue("Đơn giá(triệu)");
+            row.createCell(10, CellType.STRING).setCellValue("Thành tiền(triệu)");
+
+            for (PhieuNhap pn : list) {
+                rownum++;
+                sttPhieuNhap++;
+                row = sheet.createRow(rownum);
+
+                row.createCell(0, CellType.NUMERIC).setCellValue(sttPhieuNhap);
+                row.createCell(1, CellType.STRING).setCellValue(pn.getMaPN());
+                row.createCell(2, CellType.STRING).setCellValue(pn.getMaNCC());
+                row.createCell(3, CellType.STRING).setCellValue(pn.getMaNV()+ qlnvBUS.getNhanVien(pn.getMaNV()).getTenNV());
+                row.createCell(4, CellType.STRING).setCellValue(String.valueOf(pn.getNgayNhap()));
+                row.createCell(5, CellType.STRING).setCellValue(String.valueOf(pn.getGioNhap()));
+                row.createCell(6, CellType.NUMERIC).setCellValue(pn.getTongTien());
+
+                for (ChiTietPhieuNhap ctpn : qlctpnBUS.getAllChiTiet(pn.getMaPN())) {
+                    rownum++;
+                    row = sheet.createRow(rownum);
+
+                    String masp = ctpn.getMaSP();
+
+                    row.createCell(7, CellType.STRING).setCellValue(masp + " - " + qlspBUS.getSanPham(masp).getTenSP());
+                    row.createCell(8, CellType.NUMERIC).setCellValue(ctpn.getsLuong());
+                    row.createCell(9, CellType.NUMERIC).setCellValue(ctpn.getDonGia());
+                    row.createCell(10, CellType.NUMERIC).setCellValue(ctpn.getDonGia() * ctpn.getsLuong());
                 }
             }
             for (int i = 0; i < rownum; i++) {
--- src/GUI/FormQuanLy/QuanLyPhieuNhap.java
+++ src/GUI/FormQuanLy/QuanLyPhieuNhap.java
@@ -216,7 +216,7 @@ public class QuanLyPhieuNhap extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        new XuatExcel().xuatFileExcelHoaDon();
+        new XuatExcel().xuatFileExcelPhieuNhap();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void btnPDFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPDFActionPerformed
